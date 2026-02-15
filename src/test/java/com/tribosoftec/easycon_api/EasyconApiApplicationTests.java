3ff13d46a817eecaa82e92f7a0e38774c8933547
lucianoheric <lucianoheric55@gmail.com>
2026-02-15 16:48:32 -0300

    first commit

---
+++ src/test/java/com/tribosoftec/easycon_api/EasyconApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tribosoftec.easycon_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyconApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
